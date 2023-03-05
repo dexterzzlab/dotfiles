42aeab78cc8bc92c86703a395d18c91e623e17b9
Dexter Tan <dexter_c_tan@apple.com>
2023-03-05 02:33:01 -0800

    Added lsprestart keymap and disabled terminal keymaps

--- .config/nvim/lua/general/remaps.lua
+++ .config/nvim/lua/general/remaps.lua
@@ -7,8 +7,8 @@ vim.api.nvim_set_keymap("n", "<Space>n", ":lua vim.diagnostic.goto_next()<CR>", 
 vim.api.nvim_set_keymap("n", "<Space>N", ":lua vim.diagnostic.goto_prev()<CR>", { noremap = true })
 
 -- Normal mode remaps
-vim.api.nvim_set_keymap("n", "<Space>;", ":vsplit | terminal<CR>", { noremap = true })
-vim.api.nvim_set_keymap("n", "<Space>:", ":split | terminal<CR>", { noremap = true })
+-- vim.api.nvim_set_keymap("n", "<Space>;", ":vsplit | terminal<CR>", { noremap = true })
+-- vim.api.nvim_set_keymap("n", "<Space>:", ":split | terminal<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<Space>v", ":vsplit<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<Space>h", ":split<CR>", { noremap = true })
 vim.api.nvim_set_keymap("n", "<Space>t", ":tab sp<CR>", { noremap = true })
@@ -25,3 +25,6 @@ vim.api.nvim_set_keymap("n", "<C-n>", ":NERDTreeToggle<CR>", { noremap = true })
 
 -- " Terminal mode remaps
 vim.api.nvim_set_keymap("t", "<ESC>", [[<C-\><C-n>]], { noremap = true })
+
+-- Restart LSP
+vim.api.nvim_set_keymap("n", "<Space>R", ":LspRestart<CR>", { noremap = true })
